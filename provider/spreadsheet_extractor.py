37accf00885639c5766a6ffe2cca07269765a8f2
ToukoYui <2331631097@qq.com>
2025-10-31 14:38:17 +0800

    [ADD] first edition

--- provider/spreadsheet_extractor.py
+++ provider/spreadsheet_extractor.py
@@ -4,7 +4,7 @@ from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
 
-class spreadsheetExtractorProvider(ToolProvider):
+class SpreadsheetExtractorProvider(ToolProvider):
     
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
